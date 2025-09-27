afa9e626a0ca3f193453bcdb92766feb691498b2
huy <huy91027@gmail.com>
2025-09-27 18:08:05 +0700

    f

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -5,10 +5,10 @@ import path from "path"
 export async function GET(request: NextRequest) {
     const bus = request.nextUrl.searchParams.get("bus")
     const huong = request.nextUrl.searchParams.get("huong") || 1
-    const busstopPath = path.resolve(process.cwd(), "bus-stop", `bus_${bus}_${huong}.json`)
-    const lnglatPath = path.resolve(process.cwd(), "lnglat", `lnglat_bus_${bus}_${huong}.json`)
+    const busstopPath = path.resolve(process.cwd(), "src", "bus-stop", `bus_${bus}_${huong}.json`)
+    const lnglatPath = path.resolve(process.cwd(), "src", "lnglat", `lnglat_bus_${bus}_${huong}.json`)
 
-    
+
     let stop = readFileSync(busstopPath).toString()
     let lnglat = readFileSync(lnglatPath).toString()
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import MapLeafLet from "@/components/map-leaflet/map-leaflet";
+import MapLeafLet from "@/src/components/map-leaflet/map-leaflet";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <MapLeafLet/>
+      <MapLeafLet />
     </>
   );
 }
