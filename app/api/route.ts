6cc1c7e33d838cc9314b7e0599fa65e1ee320bee
huy <huy91027@gmail.com>
2025-09-27 17:41:24 +0700

    fix path

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,15 +1,18 @@
 import { existsSync, readFileSync } from "fs"
 import { NextRequest, NextResponse } from "next/server"
+import path from "path"
 
 export async function GET(request: NextRequest) {
     const bus = request.nextUrl.searchParams.get("bus")
     const huong = request.nextUrl.searchParams.get("huong") || 1
+    const busstopPath = path.resolve(process.cwd(), "bus-stop", `bus_${bus}_${huong}.json`)
+    const lnglatPath = path.resolve(process.cwd(), "lnglat", `lnglat_bus_${bus}_${huong}.json`)
 
-    if (!existsSync(`${process.cwd()}/bus-stop/bus_${bus}_${huong}.json`)) {
-        return  NextResponse.json({ error: 'Internal Server Error' }, { status: 404 })
+    if (!existsSync(busstopPath)) {
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 404 })
     }
-    let stop = readFileSync(`${process.cwd()}/bus-stop/bus_${bus}_${huong}.json`).toString()
-    let lnglat = readFileSync(`${process.cwd()}/lnglat/lnglat_bus_${bus}_${huong}.json`).toString()
+    let stop = readFileSync(busstopPath).toString()
+    let lnglat = readFileSync(lnglatPath).toString()
 
     return Response.json({
         stop: JSON.parse(stop),
